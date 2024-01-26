64cc7831a3b1a42eaca68780840ed8260bcd7964
budarov <albudarov@gmail.com>
2024-01-26 13:20:02 +0400

    create customer starter stuff

---
+++ customers/src/main/java/com/company/loans/customers/domain/Customer.java
@@ -0,0 +1,45 @@
+package com.company.loans.customers.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Digits;
+
+@Entity
+@Table(name = "customer")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Digits(integer = 11, fraction = 0)
+    @Column(name = "telephone")
+    private String telephone;
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
